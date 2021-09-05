be5febf1a3c7f4ad56ec65dd74819c019497c5f1
wkyle <kyle147@ymail.com>
2021-09-05 14:41:56 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div>here is wkyle's website</div>
+      <Navigation />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,12 @@
+import React, {useEffect,useState, useMemo} from "react"
+
+const Navigation = ({},context)=>{
+
+
+    return (
+        <div>
+            this is navagation bar
+        </div>
+    )
+}
+export default Navigation
