62c52731b0e44ca97b206e0350b9c9ba4e310aae
wkyle251 <1155161938@link.cuhk.edu>
2023-01-20 03:41:49 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -16,12 +16,12 @@ echo > .nojekyll
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -B main
+git checkout -B gh-pages
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:wkyle251/wkyle251.github.io.git main
+git push -f git@github.com:wkyle251/wkyle251.github.io.git gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
